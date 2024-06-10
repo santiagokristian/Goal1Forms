7f885134e5230c0a8f2695279788bfea79f15094
Kristian Francis Santiago <94884933+santiagokristian@users.noreply.github.com>
2024-06-11 00:33:19 +0800

    [INIT] Returned the AuthGuard on route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, mapToCanActivate } from '@angular/router';
 import { ApplicationFormComponent } from './module/application-form/application-form.component';
-import { ApplicationSuccessComponent } from './module/application-success/application-success.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: ApplicationFormComponent, pathMatch: 'full' },
-  { path: 'home', component: ApplicationFormComponent},
-  { path:'success', loadComponent:()=>import('./module/application-success/application-success.component').then((m)=> m.ApplicationSuccessComponent),},
+  { path: 'home', component: ApplicationFormComponent },
+  { path: 'success', loadComponent: () => import('./module/application-success/application-success.component').then((m) => m.ApplicationSuccessComponent), canActivate: mapToCanActivate([AuthGuardService]) },
   { path: '**', redirectTo: "/home" }
 ];
 
